cc894d4c1d35c0d36db3d1036e38f4c937634c08
Olivier Seret <os7@sanger.ac.uk>
2020-02-10 21:02:13 +0000

    fixing travis

--- install-travis-dependencies.sh
+++ install-travis-dependencies.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
 start_dir=$(pwd)
-cpanm --local-lib=${PWD}/perl_modules local::lib && eval $(perl -I ${PWD}/perl_modules/lib/perl5/ -Mlocal::lib)
+cpanm --local-lib=${PWD}/perl_modules local::lib && eval $(perl -I ${PWD}/perl_modules/lib/perl5/ -Mlocal::lib=${PWD}/perl_modules)
 
-cpanm --notest Dist::Zilla
+cpanm --notest \
+  Dist::Zilla \
+  Module::Install \
+  MooseX::App@1.33
 
 git clone https://github.com/sanger-pathogens/Bio-Metagenomics.git
 git clone https://github.com/sanger-pathogens/Bio-Track-Schema.git
 git clone https://github.com/sanger-pathogens/Bio-Sequencescape-Schema.git
 
-cd Bio-Metagenomics
-mkdir -p build/fake_bin && cd build/fake_bin
-touch kraken kraken-build kraken-report merge_metaphlan_tables.py metaphlan_hclust_heatmap.py
-chmod u+x $(ls) && export PATH=$(pwd):$PATH
-cd ../..
-dzil authordeps --missing | cpanm --notest
-dzil listdeps --missing | cpanm --notest
-dzil install
-
-cd ../Bio-Track-Schema
+cd Bio-Track-Schema
 dzil authordeps --missing | cpanm --notest
 dzil listdeps --missing | cpanm --notest
 dzil install
@@ -27,6 +21,12 @@ cd ../Bio-Sequencescape-Schema
 dzil authordeps --missing | cpanm --notest
 dzil listdeps --missing | cpanm --notest
 dzil install
+
+cd ../Bio-Metagenomics
+rm -rf t bin  lib/Bio/Metagenomics/CommandLine/ lib/Bio/Metagenomics/CreateLibrary.pm lib/Bio/Metagenomics/Genbank.pm  lib/Bio/Metagenomics/FileConvert.pm  lib/Bio/Metagenomics/TaxonRank.pm lib/Bio/Metagenomics/External/Metaphlan.pm lib/Bio/Metagenomics/External/Kraken.pm
+sed -i '14,19 d' dist.ini
+dzil install
+
 
 cd $start_dir
 dzil authordeps --missing | cpanm --notest
